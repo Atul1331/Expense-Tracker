10a491443325fbd8531ccd7d37fbfb90b7bc845c
Atul <atulsourabh083@gmail.com>
2023-02-14 16:11:43 +0530

    Added all files

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 import './App.css';
+
+import { GlobalProvider } from './context/GlobalState';
+
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
